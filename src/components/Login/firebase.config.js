a67453148d4dfb65522b0d2e497e992166f8da2d
a1barif2h <a1barif2h@gmail.com>
2020-09-15 13:48:58 +0600

    google sign in method is enebale

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC-MJdUJZKfQuroCjhHG-ukaELfMNIst4Q",
+  authDomain: "burj-al-arab-master-3ccb0.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-master-3ccb0.firebaseio.com",
+  projectId: "burj-al-arab-master-3ccb0",
+  storageBucket: "burj-al-arab-master-3ccb0.appspot.com",
+  messagingSenderId: "528184210611",
+  appId: "1:528184210611:web:a6359bc1c564ff6b5d6cc6",
+};
+
+export default firebaseConfig;
